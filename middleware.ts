7a4ebfbd7ecc8432c1afa5413aeb231990e54d6e
greencapitalh-cpu <greencapitalh@gmail.com>
2026-02-27 17:37:48 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,15 @@ import { locales, defaultLocale } from "./lib/locales";
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
+  // Ignore next internal paths
+  if (
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/api") ||
+    pathname.includes(".")
+  ) {
+    return NextResponse.next();
+  }
+
   if (pathname === "/") {
     const language =
       request.headers.get("accept-language")?.split(",")[0].split("-")[0];
@@ -18,3 +27,7 @@ export function middleware(request: NextRequest) {
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/((?!_next).*)"],
+};

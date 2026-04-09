7bb4be66c76b02efcf296ffbcf4e90698fe0cc89
greencapitalh-cpu <greencapitalh@gmail.com>
2026-04-09 19:24:52 -0400

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,4 +1,4 @@
-import "@/app/globals.css";
+/*import "@/app/globals.css";
 import { locales } from "@/lib/locales";
 import Navbar from "@/components/Navbar";
 
@@ -20,6 +20,36 @@ export default function LocaleLayout({
       <head />
       <body className="bg-[#0B1220] text-white antialiased">
         <Navbar locale={locale} />
+        {children}
+      </body>
+    </html>
+  );
+}
+*/
+
+
+import "@/app/globals.css";
+import { locales } from "@/lib/locales";
+import Navbar from "@/components/Navbar";
+
+export function generateStaticParams() {
+  return locales.map((locale) => ({ locale }));
+}
+
+export default function LocaleLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  const { locale } = params;
+
+  return (
+    <html lang={locale}>
+      <head />
+      <body className="bg-[#0B1220] text-white antialiased">
+        <Navbar />
         {children}
       </body>
     </html>
